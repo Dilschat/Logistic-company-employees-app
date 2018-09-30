0c85bfbbe910f91b80a1d64ca48e6bc88a911594
Dilschat <dilschat@yandex.ru>
2018-09-30 13:38:56 +0300

    hhhh

---
+++ src/main/java/com/company/Employee/app/controller/GraphsController.java
@@ -0,0 +1,27 @@
+package com.company.Employee.app.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/graph")
+public class GraphsController {
+
+    @Autowired
+    private CanvasjsChartService canvasjsChartService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String springMVC(ModelMap modelMap) {
+        List<List<Map<Object, Object>>> canvasjsDataList = canvasjsChartService.getCanvasjsChartData();
+        modelMap.addAttribute("dataPointsList", canvasjsDataList);
+        return "chart";
+    }
+
+}
---
+++ src/main/java/com/company/Employee/app/service/ProfitGraphService.java
@@ -0,0 +1,37 @@
+package com.company.Employee.app.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CanvasjsChartService {
+
+    List<List<Map<Object, Object>>> getCanvasjsChartData();
+
+}
+
+
+//CanvasjsChartServiceImpl.java
+package com.canvasjs.chart.services;
+
+        import java.util.List;
+        import java.util.Map;
+
+        import org.springframework.beans.factory.annotation.Autowired;
+
+        import com.canvasjs.chart.daos.CanvasjsChartDao;
+
+public class CanvasjsChartServiceImpl implements CanvasjsChartService {
+
+    @Autowired
+    private CanvasjsChartDao canvasjsChartDao;
+
+    public void setCanvasjsChartDao(CanvasjsChartDao canvasjsChartDao) {
+        this.canvasjsChartDao = canvasjsChartDao;
+    }
+
+    @Override
+    public List<List<Map<Object, Object>>> getCanvasjsChartData() {
+        return canvasjsChartDao.getCanvasjsChartData();
+    }
+
+}
