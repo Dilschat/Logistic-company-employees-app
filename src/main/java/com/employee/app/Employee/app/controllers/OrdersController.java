2501976b428f5a69403b62ace55165dfa909a64b
Dilschat <dilschat@yandex.ru>
2018-10-07 21:38:29 +0300

    added dummy determining type of action

--- src/main/java/com/employee/app/Employee/app/controllers/OrdersController.java
+++ src/main/java/com/employee/app/Employee/app/controllers/OrdersController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
@@ -41,7 +42,10 @@ public class OrdersController {
 
 
     @PostMapping("/next_order")
-    public String submitOrder(@ModelAttribute Order order) throws IOException {
+    public String submitOrder(@ModelAttribute Order order, @RequestParam(value="action", required=true) String action) throws IOException {
+        if(action.equals("Submit")){
+            System.out.println("Submit\n");
+        }
         DataClusterCommunication.approveOrder(order);
         return "thymeleaf/continue";
     }
