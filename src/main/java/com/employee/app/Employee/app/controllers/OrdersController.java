6279ec2b8088b343da5c1bffa57e14fdad00ca88
Dilschat <dilschat@yandex.ru>
2018-10-07 16:25:07 +0300

    Order management endpoints created

---
+++ src/main/java/com/employee/app/Employee/app/controllers/OrdersController.java
@@ -0,0 +1,39 @@
+package com.employee.app.Employee.app.controllers;
+
+import com.employee.app.Employee.app.service.DataClusterCommunication;
+import com.employee.app.Employee.app.service.daemons.OrdersToApproveQueue;
+import com.employee.app.Employee.app.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Controller
+public class OrdersController {
+
+    private final OrdersToApproveQueue ordersQueue;
+
+    @Autowired
+    public OrdersController(OrdersToApproveQueue ordersQueue) {
+        this.ordersQueue = ordersQueue;
+    }
+
+
+    @GetMapping("/next_order")
+    public ModelAndView nextOrder(ModelAndView orderView){
+        Order order = ordersQueue.getOrder();
+        orderView.addObject(order);
+        orderView.setViewName("thymeleaf/addOrder");
+        return orderView;
+    }
+
+    @PostMapping("/next_order")
+    public String submitOrder(@ModelAttribute Order order) throws IOException {
+        DataClusterCommunication.approveOrder(order);
+        return "thymeleaf/continue";
+    }
+}
