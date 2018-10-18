c5c92a9d3179fb5e6261bebb56233cefe12fc57a
Dilschat <dilschat@yandex.ru>
2018-10-18 09:08:37 +0300

    models added

--- src/main/java/com/employee/app/Employee/app/controllers/DispatchedOrdersController.java
+++ src/main/java/com/employee/app/Employee/app/controllers/DispatchedOrdersController.java
@@ -7,9 +7,12 @@ import com.employee.app.Employee.app.service.daemons.OrdersToApproveQueue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -29,11 +32,22 @@ public class DispatchedOrdersController {
         return orderView;
     }
 
+
     @GetMapping(value = "/dispatched_orders", params = {"id"})
     public ModelAndView getOrderDetails(ModelAndView orderView, @RequestParam(value = "id") String id){
-        DispatchedOrder order = ordersService.geDispatchedOrder(id);
+        DispatchedOrder order = new DispatchedOrder();
         orderView.addObject(order);
         orderView.setViewName("thymeleaf/dispatchedOrderDetails");
         return orderView;
+    }
+
+    @PostMapping(value = "/dispatched_orders")
+    public String submitOrderDetails(@ModelAttribute Order order){
+        try {
+            DataClusterCommunication.approveOrder(order);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "thymeleaf/continue";
     }
 }
--- src/main/java/com/employee/app/Employee/app/model/DispatchedOrder.java
+++ src/main/java/com/employee/app/Employee/app/model/DispatchedOrder.java
@@ -1,4 +1,54 @@
 package com.employee.app.Employee.app.model;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Currency;
+
 public class DispatchedOrder extends Order {
+    private Duration durationOfDelivering;
+    private BigDecimal profitValue;
+    private Currency profitCurrency;
+    private String clientFeedback;
+
+    public DispatchedOrder(){};
+
+    public DispatchedOrder(Duration durationOfDelivering, BigDecimal profitValue, Currency profitCurrency, String clientFeedback) {
+        super();
+        this.durationOfDelivering = durationOfDelivering;
+        this.profitValue = profitValue;
+        this.profitCurrency = profitCurrency;
+        this.clientFeedback = clientFeedback;
+    }
+
+    public Duration getDurationOfDelivering() {
+        return durationOfDelivering;
+    }
+
+    public void setDurationOfDelivering(Duration durationOfDelivering) {
+        this.durationOfDelivering = durationOfDelivering;
+    }
+
+    public BigDecimal getProfitValue() {
+        return profitValue;
+    }
+
+    public void setProfitValue(BigDecimal profitValue) {
+        this.profitValue = profitValue;
+    }
+
+    public Currency getProfitCurrency() {
+        return profitCurrency;
+    }
+
+    public void setProfitCurrency(Currency profitCurrency) {
+        this.profitCurrency = profitCurrency;
+    }
+
+    public String getClientFeedback() {
+        return clientFeedback;
+    }
+
+    public void setClientFeedback(String clientFeedback) {
+        this.clientFeedback = clientFeedback;
+    }
 }
